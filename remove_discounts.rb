54f7571e7375b73f5e6863ea4e14dfeeaa63e5b3
Rick Davies <rick@tricky3.co.uk>
2016-04-29 22:42:04 -0700

    Adds RemoveDiscounts class
    @gavinballard does this make any sense?

---
+++ remove_discounts.rb
@@ -0,0 +1,50 @@
+# RemoveDiscounts
+# ===============
+#
+# The `RemoveDiscounts` removes discounts from item prices.
+#
+# Example
+# -------
+#   * Discounts not applicable
+#
+class RemoveDiscounts
+
+  # Initializes the discount.
+  #
+  # Arguments
+  # ---------
+  #
+  # * message
+  #   The message to show for the discount.
+  #
+  def initialize(message)
+    @message = message
+  end
+
+  # Removes discounts from line item.
+  #
+  # Arguments
+  # ---------
+  #
+  # * line_item
+  #   The item from which the discounts will be removed.
+  #
+  # Example
+  # -------
+  # Given `RemoveDiscounts.new("Discounts not applicable")` and $100 item with 50% off coupon code applied:
+  #
+  # The campaign will removed 50% off discount, for $100 original price.
+  #
+  def apply(line_item)
+    # Reset discounted line price
+    new_line_price = line_item.original_line_price
+
+    # Apply the new/reset line price to this line item with given message
+    # describing the reason, which may be displayed in cart pages and
+    # confirmation emails to describe the applied adjustment.
+    line_item.change_line_price(new_line_price, message: @message)
+
+    # Print a debugging line to the console
+    puts "Reset line item with variant #{line_item.variant.id}."
+  end
+end
